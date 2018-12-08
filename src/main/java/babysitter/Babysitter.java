da6b57abfe25bf60f9c93a0699dc6cf557bf4e89
Lisa Rees <kortirion@gmail.com>
2018-12-08 09:18:24 -0500

    family c start before 2100, end after 2100

--- src/main/java/babysitter/Babysitter.java
+++ src/main/java/babysitter/Babysitter.java
@@ -8,7 +8,8 @@ public class Babysitter {
 
 	public int compute(String family, LocalDateTime start, LocalDateTime end) {
 		LocalTime endTime = end.toLocalTime();
-		LocalTime startTime = start.toLocalTime();
+		LocalTime startTime = start.toLocalTime(); // maybe start times always get rounded down and end times always get
+													// rounded up?
 		LocalTime earliestStart = LocalTime.parse("17:00");
 		LocalTime latestEnd = LocalTime.parse("04:01");
 
@@ -16,43 +17,42 @@ public class Babysitter {
 
 		if (family.equalsIgnoreCase("A")) {
 			LocalTime rateChange = LocalTime.parse("23:00");
+			long firstElapsedMinutes = 0;
+			long secondElapsedMinutes = 0;
 
-			if (startTime.isBefore(rateChange) && endTime.isBefore(rateChange) && endTime.isAfter(earliestStart)) {
-				long elapsedMinutes = Duration.between(start, end).toMinutes();
-				pay = (int) (elapsedMinutes / 60 * 15);
-
-			} else if (startTime.isAfter(rateChange)) {
-				long elapsedMinutes = Duration.between(start, end).toMinutes();
-				pay = (int) (elapsedMinutes / 60 * 20);
-
+			if ((endTime.isBefore(rateChange) || endTime.equals(rateChange)) && endTime.isAfter(earliestStart)) {
+				firstElapsedMinutes = Duration.between(start, end).toMinutes() + 1;
+			} else if (startTime.isAfter(rateChange) || startTime.equals(rateChange) || startTime.isBefore(latestEnd)) {
+				secondElapsedMinutes = Duration.between(start, end).toMinutes() + 1;
 			} else if (startTime.isBefore(rateChange) && (endTime.isAfter(rateChange) || endTime.isBefore(latestEnd))) {
-				long firstElapsedMinutes = Duration.between(startTime, rateChange).toMinutes();
-				long secondElapsedMinutes = 0;
-
+				firstElapsedMinutes = Duration.between(startTime, rateChange).toMinutes() + 1;
 				if (endTime.isBefore(latestEnd)) { // ends after midnight
-					secondElapsedMinutes = Duration.between(rateChange, LocalTime.MAX).toMinutes();
-					secondElapsedMinutes += Duration.between(LocalTime.MIDNIGHT, endTime).toMinutes();
-				} else if (endTime.isBefore(LocalTime.MAX)) { // ends before midnight, after 11PM
-					secondElapsedMinutes = Duration.between(rateChange, endTime).toMinutes();
+					secondElapsedMinutes = Duration.between(rateChange, LocalTime.MAX).toMinutes() + 1;
+					secondElapsedMinutes += Duration.between(LocalTime.MIDNIGHT, endTime).toMinutes() + 1;
+				} else if (endTime.isBefore(LocalTime.MAX)) {
+					secondElapsedMinutes = Duration.between(rateChange, endTime).toMinutes() + 1; 
 				}
-				pay = (int) ((firstElapsedMinutes / 60 * 15) + (secondElapsedMinutes / 60 * 20));
-
 			}
+			pay = (int) ((firstElapsedMinutes / 60 * 15) + (secondElapsedMinutes / 60 * 20));
 
 		} else if (family.equalsIgnoreCase("C")) {
 			LocalTime rateChange = LocalTime.parse("21:00");
 			long firstElapsedMinutes = 0;
 			long secondElapsedMinutes = 0;
-			
-			if( endTime.isBefore(rateChange) && endTime.isAfter(earliestStart)){
-				firstElapsedMinutes = Duration.between(start, end).toMinutes();
+			if ((endTime.isBefore(rateChange) || endTime.equals(rateChange)) && endTime.isAfter(earliestStart)) {
+				firstElapsedMinutes = Duration.between(start, end).toMinutes() + 1;
+			} else if (startTime.isAfter(rateChange) || startTime.equals(rateChange) || startTime.isBefore(latestEnd)) {
+				secondElapsedMinutes = Duration.between(start, end).toMinutes() + 1;
+			} else if (startTime.isBefore(rateChange) && (endTime.isAfter(rateChange) || endTime.isBefore(latestEnd))) {
+				firstElapsedMinutes = Duration.between(startTime, rateChange).toMinutes() + 1;
+				if (endTime.isBefore(latestEnd)) { // ends after midnight
+					secondElapsedMinutes = Duration.between(rateChange, LocalTime.MAX).toMinutes() + 1;
+					secondElapsedMinutes += Duration.between(LocalTime.MIDNIGHT, endTime).toMinutes() + 1;
+				} else if (endTime.isBefore(LocalTime.MAX)) {
+					secondElapsedMinutes = Duration.between(rateChange, endTime).toMinutes() + 1;
+				}
 			}
-			else if (startTime.isAfter(rateChange) || startTime.isBefore(latestEnd) ) {
-				secondElapsedMinutes = Duration.between(start, end).toMinutes();
-			}
-			
-			pay = (int) ((firstElapsedMinutes / 60 * 21) + (secondElapsedMinutes/60 * 15));
-	
+			pay = (int) ((firstElapsedMinutes / 60 * 21) + (secondElapsedMinutes / 60 * 15));
 		}
 		return pay;
 	}
--- src/test/java/babysitter/BabysitterTest.java
+++ src/test/java/babysitter/BabysitterTest.java
@@ -50,7 +50,7 @@ public class BabysitterTest {
 	@Test
 	public void shouldCalculateWagesForFamilyAGivenStartBefore23AndEndTimeAfter23ButBeforeMidnight() {
 		LocalDateTime start = LocalDateTime.of(2018, 12, 12, 17, 0);
-		LocalDateTime end = LocalDateTime.of(2018, 12, 12, 23, 1);
+		LocalDateTime end = LocalDateTime.of(2018, 12, 12, 23, 0);
 		int pay = sophie.compute("A", start, end);
 		assertEquals(90, pay);		
 	}
@@ -58,15 +58,15 @@ public class BabysitterTest {
 	@Test
 	public void shouldCalculateWagesForFamilyAgivenStartBefore23AndEndAfterMidnight() {
 		LocalDateTime start = LocalDateTime.of(2018, 12, 12, 17, 0);
-		LocalDateTime end = LocalDateTime.of(2018, 12, 13, 02, 1);
+		LocalDateTime end = LocalDateTime.of(2018, 12, 13, 02, 0);
 		int pay = sophie.compute("A", start, end);
 		assertEquals(150, pay);
 	}
 	
 	@Test
 	public void shouldCalculateWagesForFamilyAGivenStartAfter23AndEndAfterMidnight() {
-		LocalDateTime start = LocalDateTime.of(2018, 12, 12, 23, 1);
-		LocalDateTime end = LocalDateTime.of(2018, 12, 13, 02, 1);
+		LocalDateTime start = LocalDateTime.of(2018, 12, 12, 23, 0);
+		LocalDateTime end = LocalDateTime.of(2018, 12, 13, 02, 0);
 		int pay = sophie.compute("A", start, end);
 		assertEquals(60, pay);
 	}
@@ -81,10 +81,18 @@ public class BabysitterTest {
 	
 	@Test
 	public void shouldCalculateWagesForFamilyCGivenStartAndEndAfter2100() {
-		LocalDateTime start = LocalDateTime.of(2018, 12, 12, 21, 1 );
-		LocalDateTime end = LocalDateTime.of(2018, 12, 12, 23, 1);
+		LocalDateTime start = LocalDateTime.of(2018, 12, 12, 21, 0 );
+		LocalDateTime end = LocalDateTime.of(2018, 12, 12, 23, 0);
 		int pay = sophie.compute("C", start, end);
 		assertEquals(30, pay);
+	}
+	
+	@Test
+	public void shouldCalculateWagesForFamilyCGivenStartBefore2100AndEndAfter2100() {
+		LocalDateTime start = LocalDateTime.of(2018, 12, 12, 18, 0 );
+		LocalDateTime end = LocalDateTime.of(2018, 12, 12, 23, 0);
+		int pay = sophie.compute("C", start, end);
+		assertEquals(93, pay);
 	}
 	
 }
